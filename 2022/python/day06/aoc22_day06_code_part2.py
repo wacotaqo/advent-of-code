114ec594117da54a2d41b25b7dcdc6727660daf8
wacotaqo <wacotaqo@gmail.com>
2022-12-06 11:15:51 +0100

    day06

---
+++ 2022/python/day06/aoc22_day06_code_part2.py
@@ -0,0 +1,34 @@
+# This program is an exercise/challenge from the 2022 advent of code competition
+#
+#
+import os
+from collections import Counter
+
+filename = "adventofcode2022_day06_input.txt"
+#filename = "adventofcode2022_day06_input_test.txt"
+
+def duplicates(l):
+    dups = Counter(l) - Counter(set(l))
+    return list(dups.keys())
+
+fh = open(os.path.join(os.getcwd(), filename), "r")
+assignment_input = fh.read()
+fh.close()
+
+print("Input: %s" % assignment_input)
+
+start_of_packet_marker = ''
+chars_read = 0
+
+for i in range(0, len(assignment_input)-14):
+    letters4 = assignment_input[i:i+14]
+    dups = duplicates(letters4)
+    if not(dups):
+        start_of_packet_marker = letters4
+        chars_read = i+14
+        break
+
+if chars_read > 0:
+    print("Found Start-of-packet-marker (%s) after %d characters." % (start_of_packet_marker, chars_read))
+else:
+    print("No marker found.")
