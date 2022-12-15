0df5f046617dedb780992c8aa42a758ff6d381e3
wacotaqo <wacotaqo@gmail.com>
2022-12-15 18:44:47 +0100

    aoc2022 day15

--- 2022/python/day15/aoc22_day15_code_part1.py
+++ 2022/python/day15/aoc22_day15_code_part1.py
@@ -7,7 +7,9 @@ from collections import deque
 from functools import reduce
 
 filename = "adventofcode2022_day15_input.txt"
-filename = "adventofcode2022_day15_input_test.txt"
+check_row_y = 2000000
+#filename = "adventofcode2022_day15_input_test.txt"
+#check_row_y = 10
 
 DEBUG = 1
 def debug(msg):
@@ -20,3 +22,48 @@ fh.close()
 lines = data.splitlines()
 
 print(lines)
+
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+def print_grid(grid):
+    output = []
+    for row_chars in grid:
+        print(''.join(row_chars))
+
+xmin = xmax = ymin = ymax = 0
+bx_on_check_row = set()
+sensors = []
+for line in lines:
+    parts = [p1 for subline in line.split(':') for p1 in subline.split() if '=' in p1]
+    parts = [int(p2[1].replace(',', '')) for p2 in [p1.split('=') for p1 in parts]]
+    (sx, sy), (bx, by) = (parts[0], parts[1]), (parts[2], parts[3])
+    if sensors == []:
+        xmin = xmax = sx
+        ymin = ymax = sy
+    if min([sx, bx]) < xmin: xmin = min([sx, bx])
+    if min([sy, by]) < ymin: ymin = min([sy, by])
+    if max([sx, bx]) > xmax: xmax = max([sx, bx])
+    if max([sy, by]) > ymax: ymax = max([sy, by])
+    if by == check_row_y:
+        bx_on_check_row.add(bx)
+    sensors.append(((sx, sy), (bx, by), manhattan_distance(sx, sy, bx, by)))
+
+print("Max: (%s, %s) - (%s, %s)" % (xmin, ymin, xmax, ymax))
+print("Number of sensors: %s" % len(sensors))
+print("Row to check: %s" % check_row_y)
+print("Beacons on the 'check row': %s" % len(bx_on_check_row))
+
+check_row = set()
+for ((sx, sy), (bx, by), d) in sensors:
+    if check_row_y-d <= sy and check_row_y+d >= sy:
+        for x in range(-int(1e7), int(1e7)):
+            dm = manhattan_distance(sx, sy, x, check_row_y)
+            if dm <= d:
+                if not (x in bx_on_check_row):
+                    check_row.add(x)
+
+print("Number of positions on row %s where a beacon cannot be: %s" % (check_row_y, len(check_row)))
+
+
+
--- 2022/python/day15/aoc22_day15_code_part2.py
+++ 2022/python/day15/aoc22_day15_code_part2.py
@@ -7,7 +7,7 @@ from collections import deque
 from functools import reduce
 
 filename = "adventofcode2022_day15_input.txt"
-filename = "adventofcode2022_day15_input_test.txt"
+#filename = "adventofcode2022_day15_input_test.txt"
 
 DEBUG = 1
 def debug(msg):
@@ -15,8 +15,43 @@ def debug(msg):
         print(msg)
 
 fh = open(os.path.join(os.getcwd(), filename), "r")
-data = fh.read().strip()
+lines = fh.read().splitlines()
 fh.close()
-lines = data.splitlines()
-
 print(lines)
+
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+sensors = []
+beacons = []
+for line in lines:
+    parts = [p1 for subline in line.split(':') for p1 in subline.split() if '=' in p1]
+    parts = [int(p2[1].replace(',', '')) for p2 in [p1.split('=') for p1 in parts]]
+    (sx, sy), (bx, by) = (parts[0], parts[1]), (parts[2], parts[3])
+    sensors.append(((sx, sy), (bx, by), manhattan_distance(sx, sy, bx, by)))
+    beacons.append((bx, by))
+
+print("Number of sensors: %s" % len(sensors))
+print("Number of beacons: %s" % len(beacons))
+
+def possible_spot(cx, cy):
+    if not(0<=cx<=4000000 and 0<=cy<=4000000):
+        return False
+    # Check it is not in range of any of the sensors
+    for ((sx, sy), (bx, by), d) in sensors:
+        if manhattan_distance(sx, sy, cx, cy) <= d:
+            return False
+    return True
+
+# Check just outside each sensor:
+for ((sx, sy), (bx, by), d) in sensors:
+    #print("Point: %s" % str((sx, sy, d)))
+    for (factorx, factory) in ((-1, -1), (1, -1), (1, 1), (-1, 1)):     # Check all diagonals
+        #print("Direction: %s and distance: %s" % (str((signx, signy)), d+1))
+        for i in range(d+2): # at distance d+1
+            cx = sx + (factorx * (d+1-i))
+            cy = sy + (factory * i)
+            #print("From (%s, %s) dir (%s, %s) distance %s case %s: %s, %s" % (sx, sy, signx, signy, d+1, i, cx, cy))
+            if possible_spot(cx, cy):
+                print("Found the spot!: %s, %s" % (cx, cy))
+                print("Frequency: %s" % (cx*4000000+cy))
